20764e0e6535ea676cad52d9f6bd54999e9f0b6e
eburasovitch <burasovitch1e@gmail.com>
2023-12-01 15:11:49 +0700

    6 commit

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -6,10 +6,9 @@ import { useCallback } from "react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 
 const images = [
-  '/images/default-blue.png',
-  '/images/default-red.png',
-  '/images/default-slate.png',
-  '/images/default-green.png'
+  '/images/profile_image.png',
+  '/images/profile_image2.png',
+  '/images/profile_image3.png'
 ]
 
 interface UserCardProps {
@@ -38,7 +37,7 @@ const UserCard: React.FC<UserCardProps> = ({ name }) => {
 
   return (
     <div className="group flex-row w-44 mx-auto">
-        <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+        <div className="w-44 h-15 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
           <img draggable={false} className="w-max h-max object-contain" src={imgSrc} alt="" />
         </div>
       <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">{name}</div>
